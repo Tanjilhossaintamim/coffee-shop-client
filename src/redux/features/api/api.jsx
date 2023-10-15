7fc2b53a1bcf8c1417761c4af9b44be3e7a8cccc
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-10-15 11:44:47 +0600

    added server url

--- src/redux/features/api/api.jsx
+++ src/redux/features/api/api.jsx
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://coffe-shop-server-a7r26rfbv-tanjilhossain00.vercel.app",
+  }),
   endpoints: () => ({}),
 });
